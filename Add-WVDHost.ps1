7b9540748f159532a196037d17dab50396524e4b
WVDGuy <75583679+WVDGuy@users.noreply.github.com>
2021-04-29 17:02:23 +0200

    Update Add-WVDHost.ps1

--- Add-WVDHost.ps1
+++ Add-WVDHost.ps1
@@ -12,7 +12,7 @@
     Author     : InSpark
     Version    : v1.0.0
 .EXAMPLE
-    .\add-WVDHost.ps1
+    .\add-WVDHost.ps1 -wvdRegistrationKey <yourRegistrationKey>
 .DISCLAIMER
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #>
@@ -57,6 +57,3 @@ LogWriter("Installing WVD boot loader - current path is ${PSScriptRoot}")
 Start-Process -wait -FilePath $BootLoaderInstaller -ArgumentList "/q"
 LogWriter("Installing WVD agent")
 Start-Process -wait -FilePath $AgentInstaller -ArgumentList "/q RegistrationToken=${wvdRegistrationKey}"
-
-#LogWriter("Finally restarting session host")
-#Restart-Computer -Force
