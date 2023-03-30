1616bd1d3c74ef5a13163937e65709f40d8b2a65
Siebren Mossel <75583679+AVDGuy@users.noreply.github.com>
2023-03-30 11:45:08 +0200

    Update Install-ManagementApplications.ps1

--- Install-ManagementApplications.ps1
+++ Install-ManagementApplications.ps1
@@ -35,10 +35,39 @@ Install-Module -Name PowerShellGet -Force
 Add-WindowsCapability -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0" -Online
 Add-WindowsCapability -Name "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0" -Online
 
-#Download and extract AzFilesHybrid files
-$client2 = new-object System.Net.WebClient
-$client2.DownloadFileTaskAsync("https://github.com/Azure-Samples/azure-files-samples/releases/download/v0.2.5/AzFilesHybrid.zip","C:\temp\AzFilesHybrid.zip")
+#Download files and extract AzFilesHybrid files
+$client = new-object System.Net.WebClient
+$client.DownloadFileTaskAsync("https://github.com/Azure-Samples/azure-files-samples/releases/download/v0.2.5/AzFilesHybrid.zip","C:\Temp\AzFilesHybrid.zip")
+$client.DownloadFileTaskAsync("https://download.microsoft.com/download/8/d/d/8ddd685d-7d55-42e2-9555-6ab365050734/Administrative%20Templates%20(.admx)%20for%20Windows%2011%20September%202022%20Update.msi","C:\Temp\Windows11ADMX.msi")
+$client.DownloadFileTaskAsync("https://aka.ms/fslogix_download","C:\Temp\FSLogix.zip")
+$client.DownloadFileTaskAsync("https://download.microsoft.com/download/2/E/E/2EEEC938-C014-419D-BB4B-D184871450F1/admintemplates_x64_5391-1000_en-us.exe","C:\Temp\admintemplates_x64_5391-1000_en-us.exe")
+$client.DownloadFileTaskAsync("https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/ReaderADMTemplate.zip","C:\Temp\ReaderADMTemplate.zip")
+
+#Extract AzFilesHybrid files
 Expand-Archive -Path .\AzFilesHybrid.zip .\
+
+#Rename PolicyDefinitions to PolicyDefitionsOld
+Rename-Item C:\Windows\PolicyDefinitions C:\Windows\PolicyDefinitionsOld
+
+#Install Windows 11 ADMX Files and copy to PolicyDefitions
+msiexec /i C:\Temp\Windows11ADMX.msi /qn
+Robocopy /S 'C:\Program Files (x86)\Microsoft Group Policy\Windows 11 September 2022 Update (22H2)\PolicyDefinitions' C:\Windows\PolicyDefinitions
+
+#FSLogix ADMX
+Expand-Archive -Path C:\Temp\FSLogix.zip C:\Temp\FSLogix
+Copy-Item C:\Temp\FSLogix\fslogix.admx C:\Windows\PolicyDefinitions
+Copy-Item C:\Temp\FSLogix\fslogix.adml C:\Windows\PolicyDefinitions\en-us
+
+#Office 365 ADMX
+C:\Temp\admintemplates_x64_5391-1000_en-us.exe /extract:C:\Temp\ADMX /quiet
+Robocopy /S C:\Temp\ADMX\admx C:\Windows\PolicyDefinitions
+
+#Adobe Reader ADMX
+Expand-Archive -Path C:\Temp\ReaderADMTemplate.zip C:\Temp\ADMX\AdobeReaderDC
+Robocopy /S 'C:\Temp\ADMX\AdobeReaderDC' C:\Windows\PolicyDefinitions
+
+#Use Local ADMX instead of central store
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\Group Policy" /v EnableLocalStoreOverride /t REG_DWORD /d 1 /f
 
 #Copy Module to Path
 .\CopyToPSPath.ps1
