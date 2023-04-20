0015aabfbfe80a7322e8e7288c6d815aa3fc101c
Siebren Mossel <75583679+AVDGuy@users.noreply.github.com>
2023-04-20 16:45:35 +0200

    Add support for AADJoin & IntuneManaged

--- Add-AVDHost.ps1
+++ Add-AVDHost.ps1
@@ -18,36 +18,56 @@
 #>
 
 param(
-	[string] $avdRegistrationKey,
-	[string] $LogDir="$env:windir\system32\logfiles"
+    [string] $avdRegistrationKey,
+    [string] $LogDir = "$env:windir\system32\logfiles",
+    [bool] $isAzureADJoined = $false,
+    [bool] $isIntuneManaged = $false	
 )
 
 #Set Variables
 $RootFolder = "C:\AVDInstall\"
-$BootLoaderInstaller = $RootFolder+"Microsoft.RDInfra.RDAgentBootLoader.msi"
-$AgentInstaller = $RootFolder+"Microsoft.RDInfra.RDAgent.msi"
+$BootLoaderInstaller = $RootFolder + "Microsoft.RDInfra.RDAgentBootLoader.msi"
+$AgentInstaller = $RootFolder + "Microsoft.RDInfra.RDAgent.msi"
 
 
-function LogWriter($message)
-{
-	write-host($message)
-	if ([System.IO.Directory]::Exists($LogDir)) {write-output($message) | Out-File $LogFile -Append}
+function LogWriter($message) {
+    write-host($message)
+    if ([System.IO.Directory]::Exists($LogDir)) { write-output($message) | Out-File $LogFile -Append }
 }
 
 # Define logfile
-$LogFile=$LogDir+"\WVD.addAVDHost.log"
+$LogFile = $LogDir + "\WVD.addAVDHost.log"
 
 #Create Folder structure
-if (!(Test-Path -Path $RootFolder)){New-Item -Path $RootFolder -ItemType Directory}
+if (!(Test-Path -Path $RootFolder)) { New-Item -Path $RootFolder -ItemType Directory }
+
+
+if ($isIntuneManaged) {
+    Write-Log "Azure ad join preview flag enabled"
+    $registryPath = "HKLM:\SOFTWARE\Microsoft\RDInfraAgent\AzureADJoin"
+    if (Test-Path -Path $registryPath) {
+        LogWriter("Setting reg key JoinAzureAd")
+        New-ItemProperty -Path $registryPath -Name JoinAzureAD -PropertyType DWord -Value 0x01
+    }
+    else {
+        LogWriter("Creating path for azure ad join registry keys: $registryPath")
+        New-item -Path $registryPath -Force | Out-Null
+        LogWriter("Setting reg key JoinAzureAD")
+        New-ItemProperty -Path $registryPath -Name JoinAzureAD -PropertyType DWord -Value 0x01
+    }
+    if ($MdmId) {
+        LogWriter("Setting reg key MDMEnrollmentId")
+        New-ItemProperty -Path $registryPath -Name MDMEnrollmentId -PropertyType String -Value "0000000a-0000-0000-c000-000000000000"
+    }
+}
 
 #Download all source file async and wait for completion
 LogWriter("Download WVD Agent & bootloader")
 $files = @(
-    @{url = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"; path = $AgentInstaller}
-    @{url = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH"; path = $BootLoaderInstaller}
+    @{url = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"; path = $AgentInstaller }
+    @{url = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH"; path = $BootLoaderInstaller }
 )
-$workers = foreach ($f in $files)
-{ 
+$workers = foreach ($f in $files) { 
     $wc = New-Object System.Net.WebClient
     Write-Output $wc.DownloadFileTaskAsync($f.url, $f.path)
 }
